249196c1fb8af1fc70a8587e66e63362153b326b
doTryCatch <rpsuper1640@gmail.com>
2025-03-26 23:29:16 +0530

    added:api for distanceMetrixCalculation

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,28 +2,32 @@ import axios from "axios";
 
 const API_KEY = "5b3ce3597851110001cf6248d34233557de144998a2fdbd8e7bac08e";
 const BASE_URL = "https://api.openrouteservice.org/v2/matrix/driving-car";
-
 export const getNearestMerchantsWithDistance = async (
   origin: [number, number],
   merchants: any
 ) => {
-  //   console.log(merchants);
   const destinations = merchants.map((m: any) => [
     parseFloat(m.merchantLat),
     parseFloat(m.merchantLong),
   ]);
+
   try {
-    const response = await axios.post(
-      BASE_URL,
-      {
+    const response = await fetch(BASE_URL, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: API_KEY,
+      },
+      body: JSON.stringify({
         locations: [origin, ...destinations],
         metrics: ["distance"],
-      },
-      {
-        headers: { Authorization: `Bearer ${API_KEY}` },
-      }
-    );
-    const dist = response.data.distances[0];
+      }),
+    });
+
+    const result = await response.json();
+    const dist = result.distances[0];
+
     const data = merchants.map((m, index) => ({
       merchantId: m.merchantId,
       shopName: m.shopName,
@@ -32,7 +36,7 @@ export const getNearestMerchantsWithDistance = async (
       distance: `${dist[index + 1] / 1000}km`, // Convert meters to km
     }));
 
-    return data; // Exclude self-distance
+    return data;
   } catch (error) {
     console.error("ORS API Error:", error);
     return null;
